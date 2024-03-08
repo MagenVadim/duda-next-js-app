c87eab009cf853d0d2c9b329b56bb19749d48c07
Vadim Smagin <magenvadim@gmail.com>
2024-03-08 02:39:29 +0200

    #3 The fetcing of Post data has added

--- app/page.js
+++ app/page.js
@@ -1,6 +1,28 @@
+import Link from "next/link";
 
-export default function Home() {
+async function fetchData(){
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const result = await res.json();
+  return result
+}
+
+
+export default async function Home() {
+  const res = await fetchData()
+
   return (
-    <h1>Main Page</h1>
+    <div>
+      <h1>Main Page </h1>
+      {
+        res.map((el)=>(
+          <div key={el.id} className="post">
+            <h2>{el.title}</h2>
+            <p>{el.body}</p>
+            <Link href={`/post/` + el.id}>Details</Link>
+          </div>          
+        ))
+      }
+    </div>
+    
   );
 }
