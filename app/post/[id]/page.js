e43059a77c7dd9389f5eee0560a5a7a82f7fcba7
Vadim Smagin <magenvadim@gmail.com>
2024-03-11 12:49:21 +0200

    #6 GenerateStaticParams has added

--- app/post/[id]/page.js
+++ app/post/[id]/page.js
@@ -1,29 +1,39 @@
 import PostID from "@/app/components/PostID"
 
-export async function generateMetadata({params, searchParams}){
-  const post = await fetchData(params.id)
+export async function generateStaticParams() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const data = await res.json()
+ 
+  return data.map((post) => ({
+    id: post.id.toString(),
+  }))
+}
+
+
+async function getPost(id){
+  const result = await fetch('https://jsonplaceholder.typicode.com/posts/' + id);
+  const data = result.json();
+  return data;
+}
+
+export async function generateMetadata({params}){
+  const post = await getPost(params.id)
   return{
     title: post.title,
     description: post.body
   }
 }
 
-async function fetchData(id){
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts/' + id);
-  const result = await res.json();
-  return result
-}
 
-
-const Post = async ({params: {id}}) => {
-  const res = await fetchData(id)
+const Post = async ({params:{id}}) => {
+  const post = await getPost(id);
 
   return (
     <PostID 
-       key={res.userId}
-       title={res.title}
-       body={res.body}
-       userID={res.userId}
+       key={post.userId}
+       title={post.title}
+       body={post.body}
+       userID={post.userId}
     />
   )
 }
